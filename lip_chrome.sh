a2791c6f83afaa3f22c4532f07d36d96afee11cc
Matthew Ulm <mtju74@gmail.com>
2015-06-26 23:01:11 -0500

    2015-06-26
    very late in the evening. I really am looking forward to finishing this.
    I really wan tot move it to python, and let you pick and choose things.

--- lip_chrome.sh
+++ lip_chrome.sh
@@ -2,14 +2,7 @@
 #
 # This script is designed to do the basic initial investigation for a memory image.
 # By: Matthew Ulm
-# Date: May 25, 2015
-#
-# This script was built using the SIFT workstation 3.0
-# First it will hash the memory file with MD5 and SHA1.
-# then dump pslist, pstree, psxview.
-# It will then look at the svchost processes, and compare all of them together.
-# Then it attempts to dump the svchost processes using procexedump.
-# It will then MD5 hash those files, and compare them to some online engines.
+# Date: June 26, 2015
 #
 ### Set some variables
 ##########################
@@ -28,9 +21,9 @@ FTIME="$(date)"
 ### Get some information from the user.
 #######################################
 # 
-echo "This particular script will look at the processes found within the memory file. "
-echo "First we will pull the process list after hashing the memory image "
-echo "Then I will dump the svchost processes, and compare those to each other. "
+echo "This particular script will look at the chromees found within the memory file. "
+echo "First we will pull the chrome list after hashing the memory image "
+echo "Then I will dump the svchost chromees, and compare those to each other. "
 echo " "
 echo " "
 #
@@ -68,57 +61,60 @@ read APIK
 #
 # Check for some directories
 #
-setupdir=( text evidence pdump )
-for i in "${setupdir[@]}"; do
-	if [ ! -d "$HOME/$CASE/$i" ]; then
-		mkdir -p $HOME/$CASE/$i
-	fi
+if [ ! -d "$HOME/$CASE/text" ]; then
+	echo "We seem to be missing a few files and folders needed"
+	echo "In order to make this script work properly"
+	echo ""; sleep 1;
+	echo "For now I am going to exit."
+	echo "Make sure you run the scripts in the right order"
+	exit;
+fi
 done
 echo " "
 #
 #
 # Hash the memory file
 echo "I am going to take some hashes of the memory now. ";
-echo "I am going to take some hashes of the memory now. " >> $HOME/$CASE/evidence/$CASE.process.log;
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo "I am going to take some hashes of the memory now. " >> $HOME/$CASE/evidence/$CASE.chrome.log;
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 echo "------------------------------------------------------------"
-echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.process.log
+echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.log
 echo "The file being analyzed is: $FILE ";
-echo "The file being analyzed is: $FILE" >> $HOME/$CASE/evidence/$CASE.process.log;
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo "The file being analyzed is: $FILE" >> $HOME/$CASE/evidence/$CASE.chrome.log;
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 echo "------------------------------------------------------------"
-echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.process.log
+echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.log
 #
 # MD5 hash first
 echo "I will first take an MD5 hash now";
-echo "I will first take an MD5 hash now" >> $HOME/$CASE/evidence/$CASE.process.log
-md5sum $HOME/$CASE/$FILE >> $HOME/$CASE/evidence/$CASE.process.log
+echo "I will first take an MD5 hash now" >> $HOME/$CASE/evidence/$CASE.chrome.log
+md5sum $HOME/$CASE/$FILE >> $HOME/$CASE/evidence/$CASE.chrome.log
 echo "------------------------------------------------------------"
-echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.process.log
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.log
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 #
 # Now time for the SHA1 hash.
 echo "I will take a SHA1 hash now";
-echo "I will take a SHA1 hash now" >> $HOME/$CASE/evidence/$CASE.process.log
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
-sha1sum $HOME/$CASE/$FILE >> $HOME/$CASE/evidence/$CASE.process.log
+echo "I will take a SHA1 hash now" >> $HOME/$CASE/evidence/$CASE.chrome.log
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
+sha1sum $HOME/$CASE/$FILE >> $HOME/$CASE/evidence/$CASE.chrome.log
 echo "------------------------------------------------------------"
-echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.process.log
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.log
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 #
 # Now time for the SHA256 hash.
 echo "I will take a SHA256 hash now";
-echo "I will take a SHA256 hash now" >> $HOME/$CASE/evidence/$CASE.process.log
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
-sha256sum $HOME/$CASE/$FILE >> $HOME/$CASE/evidence/$CASE.process.log
+echo "I will take a SHA256 hash now" >> $HOME/$CASE/evidence/$CASE.chrome.log
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
+sha256sum $HOME/$CASE/$FILE >> $HOME/$CASE/evidence/$CASE.chrome.log
 echo "------------------------------------------------------------"
-echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.process.log
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.log
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 #
-echo "Here are the hashes from the process module run: " >> $HOME/$CASE/evidence/memory.hash.list
-cat $HOME/$CASE/evidence/$CASE.process.log >> $HOME/$CASE/evidence/memory.hash.list
+echo "Here are the hashes from the chrome module run: " >> $HOME/$CASE/evidence/memory.hash.list
+cat $HOME/$CASE/evidence/$CASE.chrome.log >> $HOME/$CASE/evidence/memory.hash.list
 echo "" >> $HOME/$CASE/evidence/memory.hash.list
 echo "" >> $HOME/$CASE/evidence/memory.hash.list
 #
@@ -152,24 +148,24 @@ case $RESP in
 		exit;;
 esac
 echo " ";
-echo "This is the profile being used: $PRFL" >> $HOME/$CASE/evidence/$CASE.process.log;
-echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.process.log;
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " "; echo " ";
+echo "This is the profile being used: $PRFL" >> $HOME/$CASE/evidence/$CASE.chrome.log;
+echo "------------------------------------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.log;
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " "; echo " ";
 #
 #
-# Let's do our process scans to get started on our analysis
+# Let's do our chrome scans to get started on our analysis
 # First move into our CASE directory
 cd $HOME/$CASE
 #
 # Set an array and loop please.
-process=( pslist psxview pstree psscan )
-for i in "${process[@]}"; do
+chrome=( pslist psxview pstree psscan )
+for i in "${chrome[@]}"; do
 	if [ ! -f "text/$i.txt" ]; then
 		echo "$i module has been run at $(date), against the memory file."
-                echo "$i module has been run at $(date), against the memory file." >> $HOME/$CASE/evidence/$CASE.process.log
-		echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo "";
+                echo "$i module has been run at $(date), against the memory file." >> $HOME/$CASE/evidence/$CASE.chrome.log
+		echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo "";
 		$VOL -f $FILE --profile=$PRFL $i > text/$i.txt
-		echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+		echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 		sleep 1;
 	else 
 		echo "It looks as if the $i module has already been run."
@@ -177,11 +173,11 @@ for i in "${process[@]}"; do
 		sleep 1; echo " ";
 	fi
 done
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 #
 ####################################################################
 # SECTION 03
-#	Let's do First start by looking at our svchost process.
+#	Let's do First start by looking at our svchost chrome.
 #
 cd $HOME/$CASE/text;
 cat pslist.txt | grep svchost | awk '{ print $3 }' >> svchost.pids.list.working
@@ -193,30 +189,30 @@ cat svchost.parent.lists.working | sort -u >> svchost.parent.list
 rm -rf svchost.pids.list.working 
 rm -rf svchost.parent.lists.working
 #
-# Let's print information about the svchost processes found.
+# Let's print information about the svchost chromees found.
 SVCHC=($(wc -l svchost.pids.list))
-echo "There ( is - are ) $SVCHC svchost process(es) discovered within RAM."
-echo "There ( is - are ) $SVCHC svchost process(es) discovered within RAM." >> $HOME/$CASE/evidence/$CASE.process.log
-echo " " >> $HOME/$CASE/evidence/$CASE.process.log; echo " ";
+echo "There ( is - are ) $SVCHC svchost chrome(es) discovered within RAM."
+echo "There ( is - are ) $SVCHC svchost chrome(es) discovered within RAM." >> $HOME/$CASE/evidence/$CASE.chrome.log
+echo " " >> $HOME/$CASE/evidence/$CASE.chrome.log; echo " ";
 echo "Here ( is - are ) all of the svchost PIDs ";
 cat svchost.pids.list; echo " "; sleep 3;   
 # The sleep is to give you some time to read it.
 # Now to the log file.
-echo "Here ( is - are ) all of the svchost PIDSs " >> $HOME/$CASE/evidence/$CASE.process.log
-cat svchost.pids.list >> $HOME/$CASE/evidence/$CASE.process.log
+echo "Here ( is - are ) all of the svchost PIDSs " >> $HOME/$CASE/evidence/$CASE.chrome.log
+cat svchost.pids.list >> $HOME/$CASE/evidence/$CASE.chrome.log
 #
 #
-# Work with the SVChost parent processes
+# Work with the SVChost parent chromees
 SVCPC=($(wc -l svchost.parent.list))
-echo "There ( is - are ) $SVCPC unique svchost parent process(es) "
-echo "There ( is - are ) $SVCPC unique svchost parent process(es) " >> $HOME/$CASE/evidence/$CASE.process.log
+echo "There ( is - are ) $SVCPC unique svchost parent chrome(es) "
+echo "There ( is - are ) $SVCPC unique svchost parent chrome(es) " >> $HOME/$CASE/evidence/$CASE.chrome.log
 echo " " >> $HOME/$CASE/evidence/$CASE.log; echo " ";
-echo "Here ( is - are ) the svchost parent(s) processes.... ";
+echo "Here ( is - are ) the svchost parent(s) chromees.... ";
 cat svchost.parent; echo " "; echo " "; sleep 3;
 # the sleep is to give you time to read it.
 # Now for the log file again.
-echo "Here ( is - are ) the svchost parent(s) processes.... "; >> $HOME/$CASE/evidence/$CASE.process.log
-cat svchost.parent >> $HOME/$CASE/evidence/$CASE.process.log
+echo "Here ( is - are ) the svchost parent(s) chromees.... "; >> $HOME/$CASE/evidence/$CASE.chrome.log
+cat svchost.parent >> $HOME/$CASE/evidence/$CASE.chrome.log
 #
 #
 cd $HOME/$CASE/pdump; 
@@ -252,24 +248,24 @@ cat svchost.256.full >> $HOME/$CASE/evidence/$CASE.256.full;
 # Thes are for the MD5 hashes.
 mkdir vxv te th mdb;
 echo "Going to check the MD5 hashes online now. ";
-echo "Going to check the MD5 hashes online now. " >>  $HOME/$CASE/evidence/$CASE.process.svchost.log;
+echo "Going to check the MD5 hashes online now. " >>  $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 while read r; do
 	# trying to keep the timing around to 20 seconds for a hash.
 	sleep 1;
 	echo "Check $r with VX Vault.....";
-	echo "Check $r with VX Vault....." >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	echo " " >> $HOME/$CASE/evidence/$CASE.process.svchost.md5.html.log;
+	echo "Check $r with VX Vault....." >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	echo " " >> $HOME/$CASE/evidence/$CASE.chrome.svchost.md5.html.log;
 	wget --header="$HEADER" --user-agent="$UA20" "http://vxvault.siri-urz.net/ViriList.php?MD5=$r" -O "vxv/$r.vxv.html"
 	sleep 5;
 	echo "Check $r with Threat Expert.....";
-	echo "Check $r with Threat Expert....." >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
+	echo "Check $r with Threat Expert....." >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 	wget --header="$HEADER" --user-agent="$UA20" "http://www.threatexpert.com/report.aspx?md5=$r" -O "te/$r.te.html"
-	echo " " >> $HOME/$CASE/evidence/$CASE.process.svchost.sha1.html.log;
+	echo " " >> $HOME/$CASE/evidence/$CASE.chrome.svchost.sha1.html.log;
 	sleep 4;
 	echo "Check with Total Hash.....";
-	echo "Check with Total Hash....." >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
+	echo "Check with Total Hash....." >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 	wget --header="$HEADER" --user-agent="$UA20" "http://totalhash.com/search/hash:$r" -O "th/$r.th.html"
-	echo " " >> $HOME/$CASE/evidence/$CASE.process.svchost.256.html.log;
+	echo " " >> $HOME/$CASE/evidence/$CASE.chrome.svchost.256.html.log;
 	sleep 5;
 	wget --header="$HEADER" --user-agent="$UA20" "http://malwaredb.malekal.com/index.php?hash=$r" -O "mdb/$r.mdb.html"
 done < svchost.md5.list
@@ -280,10 +276,10 @@ python $DSVT -k $APIK -f svchost.md5.list;
 # For VT we are going to go a bit slower on these files.
 mkdir vt_256;
 echo "Going to check the SHA 256 hashes super quick.";
-echo "Going to check the SHA 256 hashes super quick." >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
+echo "Going to check the SHA 256 hashes super quick." >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 while read r; do
 	echo "Check $r with Virus Total ......";
-	echo "Check $r with Virus Total ......" >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
+	echo "Check $r with Virus Total ......" >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 	wget --header="$HEADER" --user-agent="UA20" "https://www.virustotal.com/en/file/$variable/analysis/" -O "vt_256/$r.vt_256.html"
 	sleep 20;
 done < svchost.256.list;
@@ -295,24 +291,24 @@ done < svchost.256.list;
 # Need to include some file parsing here so we can remove hashes that have no hits.
 # Also should look at including the other hash sets.
 ssdeep -b -a -p *.exe >> $HOME/$CASE/pdump/svchost/ssdeep.svchost.log;
-cat $HOME/$CASE/pdump/svchost/ssdeep.svchost.log >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
+cat $HOME/$CASE/pdump/svchost/ssdeep.svchost.log >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 cat $HOME/$CASE/pdump/svchost/ssdeep.svchost.log;
 echo " "; sleep 3; echo " ";
 #
 #
 for i in *.exe; do
-	echo "-----------------------------------" >> $HOME/$CASE/evidence/$CASE.process.svchost.log
-	file $i >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	# /usr/local/bin/pescan $i >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	# echo " " >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	echo "Adobe Malware CLassifier....." >> $HOME/$CASE/evidence/$CASE.process.schost.log;
-	python $ADMC -f $i -n 1 >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	python $ADMC -f $i -n 2 >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	python $ADMC -f $i -n 3 >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	python $ADMC -f $i -n 4 >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-        python $ADMC -f $i >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	echo " " >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
-	echo " " >> $HOME/$CASE/evidence/$CASE.process.svchost.log;
+	echo "-----------------------------------" >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log
+	file $i >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	# /usr/local/bin/pescan $i >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	# echo " " >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	echo "Adobe Malware CLassifier....." >> $HOME/$CASE/evidence/$CASE.chrome.schost.log;
+	python $ADMC -f $i -n 1 >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	python $ADMC -f $i -n 2 >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	python $ADMC -f $i -n 3 >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	python $ADMC -f $i -n 4 >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+        python $ADMC -f $i >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	echo " " >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
+	echo " " >> $HOME/$CASE/evidence/$CASE.chrome.svchost.log;
 	strings -a -e l $i >> $i.strings
 	echo "----------" >> $i.strings; echo "----------" >> $i.strings;
 	strings -a -e b $i >> $i.strings;
